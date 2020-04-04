8b6c8d0a1e2648b077741d8ff230ea8b608ebc64
cwiede <christian.wiedemann@capgemini.com>
2020-04-04 22:39:31 +0200

    format

--- excample.tf
+++ excample.tf
@@ -5,6 +5,6 @@ provider "aws" {
 
 resource "aws_instance" "example" {
   //ami           = "ami-04d5cc9b88f9d1d39" //eu-west-1
-  ami = "ami-0cb790308f7591fa6" //eu-west2
+  ami           = "ami-0cb790308f7591fa6" //eu-west2
   instance_type = "t2.micro"
 }
